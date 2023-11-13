1c94f1e4d7a2c673c8f31f0ce222f363ec4b1b74
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2023-11-12 19:53:39 -0600

    Added the unittesting file
    Unitest of ObtieneValencia and DivisibleTempo method

---
+++ UnitTest.py
@@ -0,0 +1,22 @@
+import unittest
+from Examen2 import MiClase
+
+class TestMiClaseMethods(unittest.TestCase):
+
+    def setUp(self):
+        #Creation of the object to use the clases and make the unittesting
+        self.objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+
+    def test_ObtieneValencia(self):
+        #Test of ObtieneValencia method
+        self.assertEqual(self.objeto.ObtieneValencia(1234567), 4)
+        self.assertEqual(self.objeto.ObtieneValencia(894598765432), 6)
+
+    def test_DivisibleTempo(self):
+        #Test of DivisibleTempo method
+        self.assertEqual(self.objeto.DivisibleTempo(10), [1, 2, 5, 10])
+        self.assertEqual(self.objeto.DivisibleTempo(35), [1, 5, 7, 35])
+        # Agrega más pruebas según sea necesario
+
+if __name__ == '__main__':
+    unittest.main()
