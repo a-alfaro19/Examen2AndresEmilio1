8a1315f5ecaab810e5b6919f9ccaee4c16a28d50
a-alfaro19 <a.alfaro@estudiantec.cr>
2023-11-12 20:10:31 -0600

    [ADD] Unit test for ObtieneMasBailable and VerificaListaCanciones

--- UnitTest.py
+++ UnitTest.py
@@ -16,7 +16,16 @@ class TestMiClaseMethods(unittest.TestCase):
         #Test of DivisibleTempo method
         self.assertEqual(self.objeto.DivisibleTempo(10), [1, 2, 5, 10])
         self.assertEqual(self.objeto.DivisibleTempo(35), [1, 5, 7, 35])
-        # Agrega más pruebas según sea necesario
+
+    def test_ObtieneMasBailable(self):
+        #Test of ObtieneMasBailable method
+        self.assertEqual(self.objeto.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)
+        self.assertEqual(self.objeto.ObtieneMasBailable([0.3, 0.10, 0.79]), 0.79)
+
+    def test_VerificaListaCanciones(self):
+        #Test of VerificaListaCanciones method
+        self.assertEqual(self.objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]), True)
+        self.assertEqual(self.objeto.VerificaListaCanciones(["Canción 1", "Canción 2", None]), False)
 
 if __name__ == '__main__':
     unittest.main()
